67448c8a3553396dca7542e7c9655339bb61c64a
Haze <reyzin378@gmail.com>
2026-08-31 01:27:12 +0300

    chore: sync realistic catalog photo replacements

---
+++ supabase/migrations/20260830222600_replace_studio_catalog_photos.sql
@@ -0,0 +1,20 @@
+update public.item_types set
+  image_url='https://commons.wikimedia.org/wiki/Special:Redirect/file/IPhone%2014%20Pro.jpg',
+  image_source_url='https://commons.wikimedia.org/wiki/File:IPhone_14_Pro.jpg',
+  image_credit='iGeeksBlog / Wikimedia Commons',
+  image_license='CC BY-SA (see source)'
+where slug='iphone-14-pro';
+
+update public.item_types set
+  image_url='https://commons.wikimedia.org/wiki/Special:Redirect/file/M2%20Macbook%20Air%20Midnight%20model%20-%202.jpg',
+  image_source_url='https://commons.wikimedia.org/wiki/File:M2_Macbook_Air_Midnight_model_-_2.jpg',
+  image_credit='Wikimedia Commons contributor',
+  image_license='See source page'
+where slug='macbook-air-m2';
+
+update public.item_types set
+  image_url='https://commons.wikimedia.org/wiki/Special:Redirect/file/CasiOak%20GA-2100%20MOD.jpg',
+  image_source_url='https://commons.wikimedia.org/wiki/File:CasiOak_GA-2100_MOD.jpg',
+  image_credit='ShoakWave / Wikimedia Commons',
+  image_license='CC BY-SA 4.0'
+where slug='g-shock-2100';
