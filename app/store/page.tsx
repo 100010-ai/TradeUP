f0327ca2c507a08a22299c2b3c925fee653dceb2
Haze <reyzin378@gmail.com>
2026-08-31 02:01:33 +0300

    feat: add cosmetics store page

---
+++ app/store/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from "@/components/app-shell";
+import CosmeticsStore from "@/components/cosmetics-store";
+
+export default function StorePage() {
+  return <AppShell><CosmeticsStore /></AppShell>;
+}
